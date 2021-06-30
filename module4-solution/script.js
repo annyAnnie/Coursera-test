b448128495ff56baa3bfc48c556621b4d36456fb
Qurat ul ain Fatima <anyani.annie@gmail.com>
2021-06-30 12:47:39 +0500

    Updates

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -2,11 +2,11 @@ var names=new Array();
 names[0]="Yaakov";
 names[1]="John";
 names[2]="Jen";
-names[3]="jason";
+names[3]="annie";
 names[4]="paul";
 names[5]="frank";
 names[6]="larry";
-names[7]="paula";
+names[7]="quratulain";
 names[8]="laura";
 names[9]="jim";
 
